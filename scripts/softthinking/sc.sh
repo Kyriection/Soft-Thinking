eb38f43716d2d51744045701f7263787e2d271a4
Allen <39230030+Kyriection@users.noreply.github.com>
2025-09-12 20:19:37 -0500

    Create sc.sh

---
+++ scripts/softthinking/sc.sh
@@ -0,0 +1,8 @@
+nohup bash scripts/softthinking/launch.sh 7b_aime 0 > logs_st_baseline_7b_aime.log 2>&1 &
+nohup bash scripts/softthinking/launch.sh 1.5b_aime 1 > logs_st_baseline_1.5b_aime.log 2>&1 &
+nohup bash scripts/softthinking/launch.sh 7b_aime25 2 > logs_st_baseline_7b_aime25.log 2>&1 &
+# nohup bash scripts/softthinking/launch.sh 1.5b_aime25 0 > logs_st_baseline_1.5b_aime25.log 2>&1 &
+# nohup bash scripts/softthinking/launch.sh 7b_amc23 1 > logs_st_baseline_7b_amc23.log 2>&1 &
+# nohup bash scripts/softthinking/launch.sh 1.5b_amc23 2 > logs_st_baseline_1.5b_amc23.log 2>&1 &
+# nohup bash scripts/softthinking/launch.sh 7b_math500 0 > logs_st_baseline_7b_math500.log 2>&1 &
+# nohup bash scripts/softthinking/launch.sh 1.5b_math500 1 > logs_st_baseline_1.5b_math500.log 2>&1 &
